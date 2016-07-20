01b860f59d5c5e2de74c52a38a075f1af8f0505f
Eric Citaire <ecitaire@mediametrie.fr>
2016-07-20 18:41:49 +0200

    Create a deploy script

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+organization="ericcitaire"
+project_repo="test-travis"
+site_repo="test-travis-site"
+
+build_dir="${PWD}/target"
+
+encrypted_key="${encrypted_6b7ea0692d18_key}"
+encrypted_iv="${encrypted_6b7ea0692d18_iv}"
+
+openssl aes-256-cbc -K "${encrypted_key}" -iv "${encrypted_iv}" -in id_rsa.enc -out id_rsa -d
+chmod 400 id_rsa
+
+cat <<EOT > ~/.ssh/config
+host github.com
+ HostName github.com
+ IdentityFile ${PWD}/id_rsa
+ User git
+EOT
+
+git clone "git@github.com:${organization}/${site_repo}.git"
+
+cd "${site_repo}"
+
+if [ -d demo ] ; then
+  rm -rf demo
+fi
+mkdir demo
+cd demo
+
+unzip "${build_dir}/travis-test-1.0-SNAPSHOT.war" -x "META-INF/*" "WEB-INF/*"
+
+git add demo
+git commit -m "Update demo"
+git push
+
